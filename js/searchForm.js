883d65e88a3ce3ab3db91134c4bef5b925b7b292
HectorV <hectorfabiovasquez@gmail.com>
2024-10-16 20:18:15 -0500

    Added reactive search feature

--- js/searchForm.js
+++ js/searchForm.js
@@ -5,21 +5,17 @@ export default function createSearchForm(renderMovieCards) {
         id="search-input"
         class="form-control mr-sm-2 w-25"
         type="search"
-        placeholder="Buscar"
+        placeholder="Búsqueda por título"
         aria-label="Buscar"
       />
-      <button class="btn btn-outline-success my-3" type="submit">Filtrar</button>
     </form>
   `;
   document.getElementById("search-form").innerHTML = form;
 
-  const searchForm = document.getElementById("search-form");
   const searchInput = document.getElementById("search-input");
 
-  searchForm.addEventListener("submit", function (e) {
-    e.preventDefault(); // Evita que el formulario se envíe
-
+  searchInput.addEventListener("input", function () {
     const query = searchInput.value.toLowerCase();
-    renderMovieCards(query); // Pasamos el término de búsqueda para filtrar las películas
+    renderMovieCards(query); //llamamos la función de renderizado con el término de búsqueda
   });
 }
